fa706258353747a5028defa43d285b8516b1a628
Junez <berniskofancy10.bf10.bf@gmail.com>
2024-05-15 00:29:55 +0700

    lastest

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,52 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      'neulis': ["Neulis"],
+    extend: {
+      fontFamily: {
+        'neulisAlt-SemiBold': ["NeulisAlt-SemiBold"],
+        'neulisAlt-Reguler': ["NeulisAlt-Reguler"],
+        'neulisAlt-Reguler2': ["NeulisAlt-Reguler2"],
+        'neulisAlt-Medium': ["NeulisAlt-Medium"],
+        'neulis-Bold': ["Neulis-Bold"],
+        'neulis-SemiBold': ["Neulis-SemiBold"],
+        'monserrat-bold': ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        'hijau':{
+          100: "#D7DDC2",
+          200: "#A3B372",
+          300: "#4E7E41",
+        },
+        'putih': "#FBFEFF",
+      },
+      fontSize:{
+        navbar: ['1.125rem', {
+          lineHeight: '23px',
+          letterSpacing: '-0.01em',
+          fontWeight: 400,
+        }],
+        navbar2: ['14px', {
+          lineHeight: '20px',
+          letterSpacing: '-0.01em',
+          fontWeight: 400,
+        }],
+        button: ['16px', {
+          lineHeight: '24px',
+          letterSpacing: '-0.01em',
+          fontWeight: 700,
+        }],
+        head1: ['64px', {
+          lineHeight: '84px',
+          letterSpacing: '-0.01em',
+          fontWeight: 700,
+        }],
+        buttonExplore: ['24px', {
+          lineHeight: '36px',
+          letterSpacing: '-0.01em',
+          fontWeight: 400,
+        }],
+      },
     },
-    colors: {
-      'hijau':{
-        100: "#D7DDC2",
-        200: "#A3B372",
-        300: "#4E7E41",
-      }
-    },
-    fontSize:{
-      navbar: ['1.125rem', {
-        lineHeight: '23px',
-        letterSpacing: '-0.01em',
-        fontWeight: 400,
-      }],
-      navbar2: ['12px', {
-        lineHeight: '23px',
-        letterSpacing: '-0.01em',
-        fontWeight: 400,
-      }],
-    },
-    extend: {},
   },
   plugins: [],
 };
