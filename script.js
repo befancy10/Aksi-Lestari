d34acae230b8848c814a76572dbc536f87b84bbb
Junez <berniskofancy10.bf10.bf@gmail.com>
2024-05-21 19:36:19 +0700

    revise index testi button

--- script.js
+++ script.js
@@ -11,6 +11,72 @@ document.getElementById('scrollAbout').addEventListener('click', function() {
     });
 });
 
+// button slider testi 1
+
+currentSlideID = 1;
+
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
+console.log(totalSlides)
+
+function next(){
+    if (currentSlideID < totalSlides)
+        currentSlideID++;
+        showSlide()
+}
+
+function prev(){
+    if (currentSlideID > 1)
+        currentSlideID--;
+        showSlide()
+}
+
+function showSlide(){
+    slides =document.getElementById('slider').getElementsByTagName('li')
+    for (let index = 0; index < totalSlides; index++){
+        const element = slides[index]
+        if (currentSlideID == index + 1){
+            element.classList.remove('hidden')
+        }
+        else {
+            element.classList.add('hidden')
+        }
+    }
+}
+
+// button slider testi 2
+
+currentSlideID2 = 1;
+
+sliderElement2 = document.getElementById('slider2');
+totalSlides2 = sliderElement2.childElementCount;
+console.log(totalSlides2)
+
+function next2(){
+    if (currentSlideID2 < totalSlides2)
+        currentSlideID2++;
+        showSlide2()
+}
+
+function prev2(){
+    if (currentSlideID2 > 1)
+        currentSlideID2--;
+        showSlide2()
+}
+
+function showSlide2(){
+    slides2 =document.getElementById('slider2').getElementsByTagName('li')
+    for (let index2 = 0; index2 < totalSlides2; index2++){
+        const element = slides2[index2]
+        if (currentSlideID2 == index2 + 1){
+            element.classList.remove('hidden')
+        }
+        else {
+            element.classList.add('hidden')
+        }
+    }
+}
+
 // const carousel = document.getElementById('carousel');
 // const nextButton = document.getElementById('nextButton');
 // const prevButton = document.getElementById('prevButton');
